f614e84c913f5748c377b54d4f41805cd036cffd
shubham kumar <shubhammank@gmail.com>
2023-02-07 18:19:53 +0530

    get api

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type form struct {
+	Name  string `json:"name"`
+	Title string `json:"title"`
+}
+
+var forms = []form{
+	{Title: "Blue Train", Name: "John Coltrane"},
+	{Title: "Jeru", Name: "Gerry Mulligan"},
+}
+
+func getForm(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, forms)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/get", getForm)
+	router.Run("localhost:8080")
+}
